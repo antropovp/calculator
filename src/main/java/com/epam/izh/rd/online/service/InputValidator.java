1728479a366e6b29de5460c04cd882c2df29f6db
Pavel Antropov <p.m.antropov@gmail.com>
2020-08-02 20:21:05 +0400

    Fixed hasDoubleOperators method in InputValidatorImpl

--- src/main/java/com/epam/izh/rd/online/service/InputValidator.java
+++ src/main/java/com/epam/izh/rd/online/service/InputValidator.java
@@ -5,6 +5,7 @@ public interface InputValidator {
     boolean validate(String input);
 
     boolean doesBeginWithNumber(String input);
+    boolean doesEndWithNumber(String input);
     boolean hasOnlyNumbersAndOperators(String input);
     boolean hasDoubleOperators(String input);
 }
