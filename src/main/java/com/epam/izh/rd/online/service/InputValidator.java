f1a6a47bfe10b249570b8ddecc14241825505033
Pavel Antropov <p.m.antropov@gmail.com>
2020-08-02 19:38:37 +0400

    Changed and implemented InputValidator

--- src/main/java/com/epam/izh/rd/online/service/InputValidator.java
+++ src/main/java/com/epam/izh/rd/online/service/InputValidator.java
@@ -2,11 +2,9 @@ package com.epam.izh.rd.online.service;
 
 public interface InputValidator {
 
-    boolean isCorrectInput(String input);
+    boolean validate(String input);
 
-    boolean doesBeginWithNumber();
-    boolean isOnlyNumbersAndOperators(String input);
-    boolean isWrongOperation(String input);
-    boolean isDoubleOperators(String input);
-    boolean isNegativeNumbers(String input);
+    boolean doesBeginWithNumber(String input);
+    boolean hasOnlyNumbersAndOperators(String input);
+    boolean hasDoubleOperators(String input);
 }
