fe5b6894ea8e393361b449cee56107a6c093ce05
Pavel Antropov <p.m.antropov@gmail.com>
2020-08-02 18:38:43 +0400

    Fixed MathServiceImpl

--- src/main/java/com/epam/izh/rd/online/service/MathServiceImpl.java
+++ src/main/java/com/epam/izh/rd/online/service/MathServiceImpl.java
@@ -6,13 +6,26 @@ public class MathServiceImpl implements MathService {
 
     public double calculate(List<String> expression) {
 
-        double result = 0;
+        while (expression.size() > 1) {
 
-        for (int i = 1; i < expression.size() - 1; i+=2) {
-            result = operation(result, Double.parseDouble(expression.get(i + 1)), expression.get(i).charAt(0));
+            for (int i = 0; i < expression.size(); i++) {
+
+                if (expression.get(i).matches("\\D")) {
+                    expression.set(i - 2, String.valueOf(operation(Double.parseDouble(expression.get(i - 2)),
+                            Double.parseDouble(expression.get(i - 1)), expression.get(i).charAt(0))));
+                    expression.remove(i);
+                    expression.remove(i - 1);
+                    i -= 2;
+                }
+            }
+
+            for (String element: expression) {
+                System.out.println(element);
+            }
+
         }
 
-        return result;
+        return Double.parseDouble(expression.get(0));
     }
 
     public double operation(double operand1, double operand2, char operator) {
