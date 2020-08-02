562e212c5d10045046c933e0e7df6d2578827380
Pavel Antropov <p.m.antropov@gmail.com>
2020-08-02 19:50:59 +0400

    Implemented CalculatorService

---
+++ src/main/java/com/epam/izh/rd/online/service/CalculatorServiceImpl.java
@@ -0,0 +1,24 @@
+package com.epam.izh.rd.online.service;
+
+public class CalculatorServiceImpl {
+
+    private ConsoleService consoleService = new ConsoleServiceImpl();
+    private InputValidator inputValidator = new InputValidatorImpl();
+    private ParsingService parsingService = new ParsingServiceImpl();
+    private MathService mathService = new MathServiceImpl();
+
+    private String input;
+
+    public void start() {
+
+        System.out.print("Write the expression: ");
+        input = consoleService.read();
+
+        while (!inputValidator.validate(input)) {
+            System.out.println("Invalid input. Try again.");
+            input = consoleService.read();
+        }
+
+        System.out.print("= " + mathService.calculate(parsingService.parseReversedPolish(input)));
+    }
+}
